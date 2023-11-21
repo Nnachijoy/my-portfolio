9cc3a744ff3ee968e8960b86c25ee8851c6111bc
Nnachijoy <nnachijoychima2000@gmail.com>
2023-11-21 02:26:23 +0100

    adding animation

--- src/Component/Hero.jsx
+++ src/Component/Hero.jsx
@@ -1,33 +1,81 @@
-import React from 'react'
+// import React from 'react'
+// import { Container, Row, Col } from 'react-bootstrap';
+// import blackworkspace from "../assets/blackworkspace.jpg";
+
+// const Hero = () => {
+//   return (
+//     <div className='mt-5 p-3 hero'>
+//         <Container>
+//             <Row>
+//                 <Col md={6} xm={12} className='mt-5 mb-3'>
+//                 <h2 className='fw-bolder display-4'>
+//                     <span  className="animated-bg">Frontend Developer</span> {" "}who writes clean & efficient code.
+//                 </h2>
+//                 <p>Picture me as a dedicated architect, driven by an unwavering
+//                     commitment to excellence. Nnachi is poised with a fusion of creativity and 
+//                     precision to turn ordinary concepts into extraordinary digital
+//                     experiences. Fueled by a passion for innovation and sustained 
+//                     by a careful blend of strategic thinking, I embark on every project
+//                     with an unwavering commitment to sculpt a web masterpiece that not only
+//                     meets but exceeds expectations.
+//                 </p>
+//                 </Col>
+
+//                 <Col>
+//                 <img src={blackworkspace} alt="" className="img-fluid w-100" />  
+//                 </Col>
+//             </Row>
+//         </Container>
+//     </div>
+//   )
+// }
+
+// export default Hero
+
+
+
+import React, { useState, useEffect } from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
 import blackworkspace from "../assets/blackworkspace.jpg";
+import { CSSTransition } from 'react-transition-group';
+import './Hero.css'; 
 
 const Hero = () => {
+  const [inProp, setInProp] = useState(false);
+
+  useEffect(() => {
+    setInProp(true);
+  }, []);
+
   return (
     <div className='mt-5 p-3 hero'>
-        <Container>
-            <Row>
-                <Col md={6} xm={12} className='mt-5 mb-3'>
+      <Container>
+        <Row>
+          <Col md={6} xm={12} className='mt-5 mb-3'>
+            <CSSTransition in={inProp} timeout={3000} classNames="slide-in">
+              <div>
                 <h2 className='fw-bolder display-4'>
-                    <span  className="animated-bg">Frontend Developer</span> {" "}who writes clean & efficient code.
+                  <span className="animated-bg">Frontend Developer</span> {" "}who writes clean & efficient code.
                 </h2>
                 <p>Picture me as a dedicated architect, driven by an unwavering
-                    commitment to excellence. Nnachi is poised with a fusion of creativity and 
-                    precision to turn ordinary concepts into extraordinary digital
-                    experiences. Fueled by a passion for innovation and sustained 
-                    by a careful blend of strategic thinking, I embark on every project
-                    with an unwavering commitment to sculpt a web masterpiece that not only
-                    meets but exceeds expectations.
+                  commitment to excellence. Nnachi is poised with a fusion of creativity and
+                  precision to turn ordinary concepts into extraordinary digital
+                  experiences. Fueled by a passion for innovation and sustained
+                  by a careful blend of strategic thinking, I embark on every project
+                  with an unwavering commitment to sculpt a web masterpiece that not only
+                  meets but exceeds expectations.
                 </p>
-                </Col>
+              </div>
+            </CSSTransition>
+          </Col>
 
-                <Col>
-                <img src={blackworkspace} alt="" className="img-fluid w-100" />  
-                </Col>
-            </Row>
-        </Container>
+          <Col>
+            <img src={blackworkspace} alt="" className="img-fluid w-100" />
+          </Col>
+        </Row>
+      </Container>
     </div>
-  )
+  );
 }
 
-export default Hero
+export default Hero;
